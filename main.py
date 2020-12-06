af71dbc65006eee0fae2de5d9280a50dbfd6f917
Ludokir <stepanmedinsky@yandex.ru>
2020-12-06 12:08:39 +1000

    -

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ pygame.init()
 
 W_S, H_S = 1200, 800
 
+num = 0
 i = 0
 speed_x = randint(5, 7)
 speed_y = speed_y_start = randint(-8, -6)
-block = 100
+block = 350
 
 BG = pygame.image.load('img/bg.jpg')
 bgs = BG.get_rect()
@@ -50,7 +51,7 @@ def move():
         speed_x = -speed_x
         speed_y = -speed_y
     if ball_rect.colliderect(platform_rect) and \
-            ball_rect.bottom <= (platform_rect.bottom - 35):
+            ball_rect.bottom <= (platform_rect.bottom - 36):
         speed_x = -speed_x
     if ball_rect.bottom >= H_S:
         i = 0
@@ -72,35 +73,56 @@ def run():
 
 
 def collide():
-    global speed_y, ball_rect
+    global speed_y, ball_rect, num
     hit_index = ball_rect.collidelist(block_list)
+    font1 = pygame.font.SysFont('Arial', 30, True, False)
+    text1 = font1.render(f'Очки: {(num)}', True, (176, 42, 43))
+    text_rect1 = text1.get_rect(center=((W_S - 70), (H_S - 100)))
     if hit_index != -1:
         hit_rect = block_list.pop(hit_index)
         hit_color = color_list.pop(hit_index)
         speed_y = -speed_y
+        num += 1
+    screen.blit(text1, text_rect1)
+
+
+def win():
+    global i
+    n = []
+    font = pygame.font.SysFont('Arial', 50, True, False)
+    text = font.render('YOU WON!', True, (176, 42, 43))
+    text_rect = text.get_rect(center=((W_S // 2), (H_S // 2)))
+    if block_list == n:
+        i = 0
+        i -= 1
+        screen.blit(text, text_rect)
 
 
 '____________________________ MAIN ____________________________'
 
 pygame.display.set_caption('Cringe Арканоид')
 screen = pygame.display.set_mode((W_S, H_S))
-pygame.mouse.set_visible(True)
+pygame.mouse.set_visible(False)
 FPS = 120
 clock = pygame.time.Clock()
 
 brick = pygame.image.load('img/brick0.png')
-block_list = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
-color_list = [(randrange(30, 256), randrange(30, 256), randrange(30, 256)) for i in range(10) for j in range(4)]
+block_list = [pygame.Rect(
+    10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
+color_list = [(randrange(30, 256), randrange(
+    30, 256), randrange(30, 256)) for i in range(10) for j in range(4)]
 
 while True:
     i += 1
     run()
     screen.blit(BG, bgs)
-    [pygame.draw.rect(screen, color_list[color], block) for color, block in enumerate(block_list)]
+    bricks = [pygame.draw.rect(screen, color_list[
+        color], block) for color, block in enumerate(block_list)]
     screen.blit(ball, ball_rect)
     screen.blit(platform, platform_rect)
     move()
     collide()
     init()
+    win()
     pygame.display.update()
     clock.tick(FPS)
